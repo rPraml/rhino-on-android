fd0755e84e50e53f8711dfde96a540fb5c4063e7
Roland Praml <pram@gmx.de>
2024-10-10 20:50:54 +0200

    Add input text and button

--- app/src/main/java/com/example/rhino/MainActivity.kt
+++ app/src/main/java/com/example/rhino/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.rhino
 
-import org.mozilla.javascript.Context;
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rhino.ui.theme.RhinoJavasciptAppTheme
+import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +34,45 @@ class MainActivity : ComponentActivity() {
         setContent {
             RhinoJavasciptAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = executeJavaScript("'javascript' + ' ' + 'works'"),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .padding(16.dp)
+                    ) {
+                        var inputText by remember { mutableStateOf("") }
+                        var outputText by remember { mutableStateOf("") }
+
+                        // TextField for user to input JavaScript
+                        TextField(
+                            value = inputText,
+                            onValueChange = { inputText = it },
+                            label = { Text("Enter JavaScript") },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Button to evaluate JavaScript
+                        Button(
+                            onClick = {
+                                // Execute the JavaScript and set the result to outputText
+                                outputText = executeJavaScript(inputText)
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text("Run JavaScript")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Display the result
+                        Text(
+                            text = "Result: $outputText",
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 }
             }
         }
